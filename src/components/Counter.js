ec9535111304caa49a6c888177bae13a3b2d6b28
justin-hill426 <jrhill426@gmail.com>
2021-09-24 11:04:59 -0600

    Change state location to be higher up

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const Counter = (props) => {  
+    const index = props.index
+    return (
+        <div className="counter">
+          <button className="counter-action decrement" onClick={() => props.changeScore(index, -1)}> - </button>
+          <span className="counter-score">{ this.score }</span>
+          <button className="counter-action increment" onClick={() => props.changeScore(index, 1)}> + </button>
+        </div>
+      );
+  }
+
+  export default Counter;
